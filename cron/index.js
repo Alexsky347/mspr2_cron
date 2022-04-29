7b7d3c83d4e529bc7b9733d94a2531b87654c8ab
Alexsky347 <47504664+Alexsky347@users.noreply.github.com>
2022-04-29 20:23:24 +0200

    Delete cron directory

--- cron/index.js
+++
@@ -1,9 +0,0 @@
-import cron from 'node-cron';
-import { getAllParking } from '../function/index.js';
-import { setCache } from '../cache/index.js';
-
-export const cronJob = () => {
-    cron.schedule(process.env.TIME_SCHEDULE, async () => {
-        setCache('testKey', await getAllParking());
-    })
-}
